04144da4730c01270488364de40d46e2e6ef2cf1
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2019-06-17 16:29:50 -0500

    Get contacts (all) is ready and new libraries added

---
+++ phone-book-back/src/services/GetContacts.java
@@ -0,0 +1,63 @@
+package services;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+import models.Contact;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import utilities.DatabaseConfiguration;
+import utilities.RestConfiguration;
+
+
+public class GetContacts {
+
+	private static ConnectionSource connectionSource;
+	
+	private static Dao<Contact, String> contactDao;
+
+	public static void main(String[] args) {
+		// Configures the database connection
+		connectionSource = DatabaseConfiguration.initializeDB();
+		
+		// Defines the port to use
+		port(RestConfiguration.getHerokuAssignedPort());
+		
+		// Initialize the cors
+		RestConfiguration.initializeCors();
+		
+		try {
+			// Creates a new DAO
+			contactDao = DaoManager.createDao(connectionSource, Contact.class);
+			
+			// Creates the table CONTACT if it doesn't exist
+			TableUtils.createTableIfNotExists(connectionSource, Contact.class);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		get("/api/getContacts", "application/json", (req, res) -> {
+			try {
+				List<Contact> contacts = contactDao.queryForAll();
+				String json = new Gson().toJson(contacts);
+				res.body(json);
+				res.status(200);
+			} catch (SQLException e) {
+				res.status(404);
+				res.body("{\"errorMessage\":\"No fue posible obtener los beneficios\", \"error\":\"" + e.getMessage() + "\"}");
+			} 
+			return res.body();
+		});
+		
+	}
+	
+}
---
+++ phone-book-back/src/utilities/RestConfiguration.java
@@ -0,0 +1,48 @@
+package utilities;
+
+import static spark.Spark.before;
+import static spark.Spark.options;
+
+public class RestConfiguration {
+	
+	/**
+	 * Initialize the CORS of the app for allowing external requests
+	 */
+	public static void initializeCors() {
+		options("/*",
+				(request, response) ->{
+
+					String accessControlRequestHeaders = request
+							.headers("Access-Control-Request-Headers");
+					if (accessControlRequestHeaders != null) {
+						response.header("Access-Control-Allow-Headers",
+								accessControlRequestHeaders);
+					}
+
+					String accessControlRequestMethod = request
+							.headers("Access-Control-Request-Method");
+					if (accessControlRequestMethod != null) {
+						response.header("Access-Control-Allow-Methods",
+								accessControlRequestMethod);
+					}
+
+					return "OK";
+				});
+
+		before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+	}
+	
+	/**
+	 * Gets the port in which the main app will run
+	 * @return Port number
+	 */
+	public static int getHerokuAssignedPort() {
+		int port = 4567;
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            port = Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        System.out.println("Running in port: " + port);
+        return port;
+    }
+}
